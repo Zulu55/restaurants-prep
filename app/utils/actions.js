fcf5707139f10c1581ca93a541bdbe48c882c347
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2021-01-27 15:03:44 -0500

    Top Restaurantas

--- app/utils/actions.js
+++ app/utils/actions.js
@@ -218,3 +218,19 @@ export const removeFromFavorite = async(idRestaurant) => {
   
     return response;
 }
+
+export const getTopRestaurants = () => {
+    db.collection("restaurants")
+    .orderBy("rating", "desc")
+    .limit(5)
+    .get()
+    .then((response) => {
+        const restaurantArray = []
+        response.forEach((doc) => {
+            const data = doc.data()
+            data.id = doc.id
+            restaurantArray.push(data)
+        })
+        return restaurantArray
+    })
+} 
